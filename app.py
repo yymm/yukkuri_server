937fa562bd7b501dba704f428454b49d04891e85
yuya yano <yuya.yano.6260@gmail.com>
2013-11-28 10:49:32 +0000

    Added files

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+import os
+import commands
+import datetime
+
+app = Flask(__name__)
+
+def yukkuri_wav(word):
+    wav_file = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + '.wav'
+    wav_file_path = './static/' + wav_file
+    os.system('./yukkuri/AquesTalkPi -v f2 -o %s %s' % (wav_file_path, word.encode('utf-8')))
+    return wav_file
+
+@app.route("/")
+def hello():
+    wav_files = commands.getoutput('ls ./static')
+    wav_list = wav_files.split()
+    return render_template('index.html', **locals())
+
+@app.route("/yukkuri/<word>")
+def echo_yukkuri(word):
+    wav_file = yukkuri_wav(word)
+    return render_template('yukkuri.html', **locals())
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='192.168.5.222', port=10001)
